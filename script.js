4715606e4973cf8ceac8a4cac6b4dd26ac5d82c8
harun <harun-626@hotmail.com>
2021-05-23 22:46:45 +0300

    Enough refactoring for today.

--- script.js
+++ script.js
@@ -9,22 +9,31 @@ randomPlayerSelection =
 
 let computerPlay = () => {
   var randomNumber = Math.random();
-  randomNumber < 1 / 3
+  return randomNumber < 1 / 3
     ? "rock"
     : randomUserNumber >= 2 / 3
     ? "paper"
     : "scissors";
 };
-var computerSelection = computerPlay();
-computerPlay();
-console.log(computerSelection);
 
-const playRound = function (playerSelection, computerSelection) {
-  var computerSelection = computerPlay();
-  var playerSelection = prompt(
+let userScore = 0;
+let computerScore = 0;
+let answers = ["rock", "paper", "scissors"];
+
+function playRound() {
+  var playerRespond = prompt(
     "Rock, Paper or Scissors?",
     `${randomPlayerSelection}`
   ).toLowerCase();
+
+  if (answers.indexOf(playerRespond) === -1) {
+    console.log(
+      "Invalid answer! Please click ok or write rock, paper or scissors."
+    );
+    return;
+  }
+
+  var computerSelection = computerPlay();
   var w = "You Win!";
   var d = "Draw!";
   var l = "You Lose!";
@@ -33,17 +42,26 @@ const playRound = function (playerSelection, computerSelection) {
     [w, d, l],
     [l, w, d],
   ];
-  var i = playerSelection == "rock" ? 0 : playerSelection == "paper" ? 1 : 2;
-  console.log(computerSelection);
-  console.log(i);
-  var j =
-    computerSelection == "rock" ? 0 : computerSelection == "paper" ? 1 : 2;
-  console.log(j);
-  alert(
-    `${arrayOfResults[i][j]}, Computer's selection was: ${computerSelection}`
+
+  i = playerRespond === answers[0] ? 0 : playerRespond === answers[1] ? 1 : 2;
+  j =
+    computerSelection === answers[0]
+      ? 0
+      : computerSelection === answers[1]
+      ? 1
+      : 2;
+
+  if (arrayOfResults[i][j] === w) {
+    userScore++;
+  } else if (arrayOfResults[i][j] === l) {
+    computerScore++;
+  }
+
+  console.log(
+    `computerSelection was: ${computerSelection}, ${arrayOfResults[i][j]}\ncurrent score; computer: ${computerScore}, your score: ${userScore}`
   );
   return arrayOfResults[i][j];
-};
+}
 
 const game = () => {
   for (let i = 0; i < 5; i++) {
